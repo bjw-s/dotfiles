7a9b54255e1c17eaac671a2e94bb11fde38a9cfd
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-03-09 09:51:27 +0100

    feat(mako): Replace with swaync

--- home/private_dot_local/bin/executable_wofi_power.sh
+++ home/private_dot_local/bin/executable_wofi_power.sh
@@ -2,11 +2,12 @@
 
 ## Shows a drop down menu with power options
 
-case "$(echo -e " Exit sway\n Power Off\n Reboot\n" | wofi --show dmenu --prompt="Select an option")" in
+case "$(echo -e " Exit sway\n Power Off\n Reboot\n Suspend\n" | wofi --show dmenu --prompt="Select an option")" in
 " Exit sway")
   swaymsg exit
   loginctl terminate-user $USER
   ;;
 " Power Off") exec systemctl poweroff -i ;;
 " Reboot") exec systemctl reboot ;;
+" Suspend") exec systemctl suspend ;;
 esac
