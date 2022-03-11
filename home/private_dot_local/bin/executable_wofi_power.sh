82bcec7d8efb14077098166699fdce684305d7f0
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-03-11 08:25:09 +0100

    feat: Add swaylock, more hotkeys

--- home/private_dot_local/bin/executable_wofi_power.sh
+++ home/private_dot_local/bin/executable_wofi_power.sh
@@ -2,7 +2,7 @@
 
 ## Shows a drop down menu with power options
 
-case "$(echo -e " Exit sway\n Power Off\n Reboot\n Suspend\n" | wofi --show dmenu --prompt="Select an option")" in
+case "$(echo -e " Exit sway\n Power Off\n Reboot\n Suspend\n Lock" | wofi --show dmenu --prompt="Select an option" --lines 5 --hide-scroll -k /dev/null)" in
 " Exit sway")
   swaymsg exit
   loginctl terminate-user $USER
@@ -10,4 +10,5 @@ case "$(echo -e " Exit sway\n Power Off\n Reboot\n Suspend\n" | wofi
 " Power Off") exec systemctl poweroff -i ;;
 " Reboot") exec systemctl reboot ;;
 " Suspend") exec systemctl suspend ;;
+" Lock" ) exec lock.sh ;;
 esac
