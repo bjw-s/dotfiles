85cc248d61e9466074cd176224d7bc06eef06fe4
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2022-04-15 08:22:20 +0200

    feat: Replace Chrome with chromium

--- home/private_dot_local/bin/executable_reset_chrome_exitstate.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-tmp=$(mktemp /tmp/tmp.XXXXXXX)
-jq '.profile.exit_type= "Normal" | .profile.exited_cleanly = true' ~/.config/google-chrome/Default/Preferences > "$tmp"
-mv "$tmp" ~/.config/google-chrome/Default/Preferences
